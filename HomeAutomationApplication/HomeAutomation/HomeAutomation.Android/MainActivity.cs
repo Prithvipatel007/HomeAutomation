757b5ac6f75b3f7ba9642ea8733e9e6b0c71b783
Prithvipatel007 <prithvipatel007@gmail.com>
2020-06-06 15:26:25 +0200

    Home automation UI added

--- HomeAutomationApplication/HomeAutomation/HomeAutomation.Android/MainActivity.cs
+++ HomeAutomationApplication/HomeAutomation/HomeAutomation.Android/MainActivity.cs
@@ -9,13 +9,19 @@ using Android.OS;
 
 namespace HomeAutomation.Droid
 {
-    [Activity(Label = "HomeAutomation", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    //[Activity(Label = "HomeAutomation", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "Home Automation", Theme = "@style/splashscreen", Icon = "@drawable/homeAutomation", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation, LaunchMode = LaunchMode.SingleTop)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
             TabLayoutResource = Resource.Layout.Tabbar;
             ToolbarResource = Resource.Layout.Toolbar;
+
+            base.Window.RequestFeature(WindowFeatures.ActionBar);
+            // Name of the MainActivity theme you had there before.
+            // Or you can use global::Android.Resource.Style.ThemeHoloLight
+            base.SetTheme(Resource.Style.MainTheme);
 
             base.OnCreate(savedInstanceState);
 
--- HomeAutomationApplication/HomeAutomation/HomeAutomation/MainPage.xaml.cs
+++ HomeAutomationApplication/HomeAutomation/HomeAutomation/MainPage.xaml.cs
@@ -5,17 +5,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Xamarin.Forms;
+using Xamarin.Forms.Internals;
 
 namespace HomeAutomation
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage : MasterDetailPage
     {
         public MainPage()
         {
             InitializeComponent();
+            Detail = new NavigationPage(new Pages.HomePage()) { BarBackgroundColor = Color.LightGray};
+            IsPresented = false;
+        }
+
+        private void LivingRoomButtonClicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pages.LivingRoom()) { BarBackgroundColor = Color.LightGray };
+            IsPresented = false;
+        }
+
+        private void StudyRoomButtonClicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pages.StudyRoom()) { BarBackgroundColor = Color.LightGray };
+            IsPresented = false;
+        }
+
+        private void KitchenButtonClicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pages.Kitchen()) { BarBackgroundColor = Color.LightGray };
+            IsPresented = false;
+        }
+
+        private void BedroomButtonClicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pages.BedRoom()) { BarBackgroundColor = Color.LightGray };
+            IsPresented = false;
+        }
+
+        private void GardenButtonClicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pages.Garden()) { BarBackgroundColor = Color.LightGray };
+            IsPresented = false;
+        }
+
+        private void EntranceButtonClicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pages.Entrance()) { BarBackgroundColor = Color.LightGray };
+            IsPresented = false;
+        }
+
+        private void HomeButtonClicked(object sender, EventArgs e)
+        {
+            Detail = new NavigationPage(new Pages.HomePage()) { BarBackgroundColor = Color.LightGray };
+            IsPresented = false;
         }
     }
 }
